498178a5f023fffb0f7e75b69b4870bdf41583c3
Boris Serebrov <serebrov@gmail.com>
2019-03-02 01:11:39 +0200

    Add axios and bootstrap.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+
 Vue.config.productionTip = false;
 
 new Vue({
