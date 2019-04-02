dcfe50f4b53630f21f26f0cd7faa20a1e2aa0e05
Boris Serebrov <serebrov@gmail.com>
2019-04-03 00:25:56 +0300

    Reformat backend code

--- backend/app/app.py
+++ backend/app/app.py
@@ -5,35 +5,30 @@ from marshmallow import ValidationError
 from app.models.forum import User
 from app.serializers.forum import UserSchema
 from app.create_app import create_app
-from app.extensions import (
-    db,
-    webargs,
-)
+from app.extensions import db, webargs
 
 
-app = create_app(os.getenv('FLASK_CONFIG') or 'local')
+app = create_app(os.getenv("FLASK_CONFIG") or "local")
 
 
 @app.route("/api")
 def hello():
-    return jsonify({
-        "message": "Hello"
-    })
+    return jsonify({"message": "Hello"})
 
 
-@app.route("/api/user/<string:id>", methods=['GET'])
+@app.route("/api/user/<string:id>", methods=["GET"])
 def user_get(id):
     user = User.query.get_or_404(id)
     return jsonify({"data": UserSchema().dump(obj=user)}), 200
 
 
-@app.route("/api/users", methods=['GET'])
+@app.route("/api/users", methods=["GET"])
 def users_get():
     users = User.query.all()
     return jsonify({"data": UserSchema().dump(users, many=True)}), 200
 
 
-@app.route("/api/users", methods=['POST'])
+@app.route("/api/users", methods=["POST"])
 def user_create():
     result = webargs.parse(UserSchema(), request)
     user = User(**result)
@@ -42,17 +37,16 @@ def user_create():
     return jsonify({"data": UserSchema().dump(obj=user)}), 201
 
 
-@app.route("/api/users/<string:id>", methods=['POST'])
+@app.route("/api/users/<string:id>", methods=["POST"])
 def user_update(id):
     user = User.query.get(id)
     if not user:
-        raise ValidationError(
-            'User not found by id: {}'.format(id), 'id')
+        raise ValidationError("User not found by id: {}".format(id), "id")
 
     # Pass user as schema context, so we can do additional
     # validation based on the user data.
     schema = UserSchema()
-    schema.context['user'] = user
+    schema.context["user"] = user
     result = webargs.parse(schema, request)
 
     # Assign validated attributes to the model.
@@ -71,11 +65,9 @@ def handle_error(error, req, schema, status_code, headers):
 
 @app.errorhandler(ValidationError)
 def validateion_error(error):
-    return jsonify({
-        "errors": error.messages
-    }), 400
+    return jsonify({"errors": error.messages}), 400
 
 
-@app.cli.command('db_create_all')
+@app.cli.command("db_create_all")
 def db_create_all():
     db.create_all()
--- backend/app/config.py
+++ backend/app/config.py
@@ -10,8 +10,8 @@ class Config:
     SQLALCHEMY_POOL_SIZE = 5
     SQLALCHEMY_MAX_OVERFLOW = 15
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI_ALEMBIC = os.getenv('SQLALCHEMY_DATABASE_URI_ALEMBIC')
-    SQLALCHEMY_DATABASE_URI_CELERY = os.getenv('SQLALCHEMY_DATABASE_URI_CELERY')
+    SQLALCHEMY_DATABASE_URI_ALEMBIC = os.getenv("SQLALCHEMY_DATABASE_URI_ALEMBIC")
+    SQLALCHEMY_DATABASE_URI_CELERY = os.getenv("SQLALCHEMY_DATABASE_URI_CELERY")
 
 
 class LocalConfig(Config):
@@ -19,9 +19,9 @@ class LocalConfig(Config):
     DEBUG = True
     JSONIFY_PRETTYPRINT_REGULAR = True
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI') or 'postgresql://localhost/forum'  # noqa
+    SQLALCHEMY_DATABASE_URI = (
+        os.getenv("SQLALCHEMY_DATABASE_URI") or "postgresql://localhost/forum"
+    )  # noqa
 
 
-config = {
-    'local': LocalConfig,
-}
+config = {"local": LocalConfig}
